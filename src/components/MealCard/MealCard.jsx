65e4c25aaad9c4ec9ac6db7ce476765f2db6ecdf
Sayon Usman <sayonusman.ma@gmail.com>
2023-04-07 05:15:01 +0300

    Meal's card design has been updated

--- src/components/MealCard/MealCard.jsx
+++ src/components/MealCard/MealCard.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const MealCard = ({ meal }) => {
   console.log(meal);
   return (
-    <div className="card w-124 glass m-2">
+    <div className="card w-124 glass m-2 rounded-lg shadow-lg shadow-zinc-800">
       <figure>
         <img src={meal.strMealThumb} alt="Meal" />
       </figure>
@@ -16,7 +16,7 @@ const MealCard = ({ meal }) => {
           perferendis eligendi ea, aut rerum odio!.
         </p>
         <div className="card-actions justify-end">
-          <button className="btn">Show Details</button>
+          <button className="btn rounded">Show Details</button>
         </div>
       </div>
     </div>
