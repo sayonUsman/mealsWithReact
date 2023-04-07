4c05fcc216855b9537291d0b531f903ca7df1ac3
Sayon Usman <sayonusman.ma@gmail.com>
2023-04-07 21:16:55 +0300

    Navbar link has been updated

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,24 +1,23 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <nav className="flex-column lg:flex lg:h-16 justify-between items-center sm:mt-3 lg:mt-5 pt-3 lg:pt-0 container mx-auto bg-zinc-800 md:text-2xl text-white font-bold sm:rounded">
       <ul className="h-full flex justify-center items-center lg:pl-5">
         <li className="pr-3 md:pr-9">
-          <Link to="/">Home</Link>
+          <a href="/">Home</a>
         </li>
 
         <li className="pr-3 md:pr-9">
-          <Link to="/about">About</Link>
+          <a href="/about">About</a>
         </li>
 
         <li className="pr-3 md:pr-9">
-          <Link to="/contact">Contact</Link>
+          <a href="/contact">Contact</a>
         </li>
 
         <li className="pr-3 md:pr-9">
-          <Link to="/services">Services</Link>
+          <a href="/services">Services</a>
         </li>
       </ul>
 
