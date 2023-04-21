f114aeff0e9e3bdf2f9279178178216da3d2136c
Sayon Usman <sayonusman.ma@gmail.com>
2023-04-21 14:38:01 +0300

    Navbar link has been updated again

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,23 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <nav className="flex-column lg:flex lg:h-16 justify-between items-center sm:mt-3 lg:mt-5 pt-3 lg:pt-0 container mx-auto bg-zinc-800 md:text-2xl text-white font-bold sm:rounded">
       <ul className="h-full flex justify-center items-center lg:pl-5">
         <li className="pr-3 md:pr-9">
-          <a href="/">Home</a>
+          <Link to="/">Home</Link>
         </li>
 
         <li className="pr-3 md:pr-9">
-          <a href="/about">About</a>
+          <Link to="/Linkbout">About</Link>
         </li>
 
         <li className="pr-3 md:pr-9">
-          <a href="/contact">Contact</a>
+          <Link to="/contact">Contact</Link>
         </li>
 
         <li className="pr-3 md:pr-9">
-          <a href="/services">Services</a>
+          <Link to="/services">Services</Link>
         </li>
       </ul>
 
@@ -28,7 +29,9 @@ const Navbar = () => {
           placeholder="Search"
         />
 
-        <button className="btn rounded mr-5">Search</button>
+        <button className="btn rounded mr-5">
+          <Link to="/login">Login</Link>
+        </button>
       </div>
     </nav>
   );
